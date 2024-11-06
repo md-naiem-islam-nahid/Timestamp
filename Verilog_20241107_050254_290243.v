7451078f8b7ed3cd61a27fbbb8e693cad3f29e7c
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:54 +0600

    Added Verilog_20241107_050254_290243.v with Verilog code and metadata

---
+++ Verilog_20241107_050254_290243.v
@@ -0,0 +1,15 @@
+module main;
+initial begin
+  $display("Hello, World!");
+  $finish;
+end
+endmodule
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Verilog
+Magic Number: 5791
+Time: 2024-11-07T05:02:54.290243
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
