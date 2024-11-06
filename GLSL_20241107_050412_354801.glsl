44fb4f4398f9eb33d5295d4768a30ac0cc3cf3d2
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:12 +0600

    Added GLSL_20241107_050412_354801.glsl with GLSL code and metadata

---
+++ GLSL_20241107_050412_354801.glsl
@@ -0,0 +1,10 @@
+// Shader code; not applicable
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: GLSL
+Magic Number: 8035
+Time: 2024-11-07T05:04:12.354801
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
