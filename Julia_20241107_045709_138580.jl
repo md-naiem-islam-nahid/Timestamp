93111d73a9368b44cea22471ac933266618095b0
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:09 +0600

    Added Julia_20241107_045709_138580.jl with Julia code and metadata

---
+++ Julia_20241107_045709_138580.jl
@@ -0,0 +1,10 @@
+println("Hello, World!")
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Julia
+Magic Number: 4297
+Time: 2024-11-07T04:57:09.138580
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
