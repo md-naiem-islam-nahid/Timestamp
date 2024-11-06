8cca4573a3eebb9ca395fcc9b4079e71e5428b2c
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:35 +0600

    Added Rust_20241107_050235_192860.rs with Rust code and metadata

---
+++ Rust_20241107_050235_192860.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, World!");
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Rust
+Magic Number: 6111
+Time: 2024-11-07T05:02:35.192860
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
