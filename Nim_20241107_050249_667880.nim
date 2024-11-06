079c935e512041caff435323002aa33c9badac78
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:50 +0600

    Added Nim_20241107_050249_667880.nim with Nim code and metadata

---
+++ Nim_20241107_050249_667880.nim
@@ -0,0 +1,10 @@
+echo "Hello, World!"
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Nim
+Magic Number: 1158
+Time: 2024-11-07T05:02:49.667880
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
