10986b45326937481c90d1e01d7665e92e6871d5
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:37 +0600

    Added Scala_20241107_050236_955903.scala with Scala code and metadata

---
+++ Scala_20241107_050236_955903.scala
@@ -0,0 +1,14 @@
+object HelloWorld {
+    def main(args: Array[String]) = {
+        println("Hello, World!")
+    }
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Scala
+Magic Number: 7652
+Time: 2024-11-07T05:02:36.955903
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
