3f51739c19c1c070cf98e289061991adc82c5cef
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:39 +0600

    Added Haxe_20241107_050339_132961.hx with Haxe code and metadata

---
+++ Haxe_20241107_050339_132961.hx
@@ -0,0 +1,14 @@
+class HelloWorld {
+  static public function main() {
+    trace('Hello, World!');
+  }
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Haxe
+Magic Number: 6993
+Time: 2024-11-07T05:03:39.132961
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
