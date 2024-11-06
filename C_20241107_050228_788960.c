6eb83f90f93d4848ddfa5f4cbd473e1152d52266
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:29 +0600

    Added C_20241107_050228_788960.c with C code and metadata

---
+++ C_20241107_050228_788960.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: C
+Magic Number: 4934
+Time: 2024-11-07T05:02:28.788960
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
