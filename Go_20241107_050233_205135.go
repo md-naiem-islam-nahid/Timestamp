4dcdf026c15bb92a09aa1864821eb60ce93d0133
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:33 +0600

    Added Go_20241107_050233_205135.go with Go code and metadata

---
+++ Go_20241107_050233_205135.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Go
+Magic Number: 9943
+Time: 2024-11-07T05:02:33.207194
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
