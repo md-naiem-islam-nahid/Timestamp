ee6bb0219441d53ae4ac84371d185d265a192d4a
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:04 +0600

    Added C#_20241107_045704_096121.cs with C# code and metadata

---
+++ C#_20241107_045704_096121.cs
@@ -0,0 +1,15 @@
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: C#
+Magic Number: 7232
+Time: 2024-11-07T04:57:04.096121
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
