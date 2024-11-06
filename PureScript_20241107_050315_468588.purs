a3536db7850886a8234758f9a5dfaadc78f0b0f6
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:15 +0600

    Added PureScript_20241107_050315_468588.purs with PureScript code and metadata

---
+++ PureScript_20241107_050315_468588.purs
@@ -0,0 +1,14 @@
+module Main where
+
+import Prelude
+
+main = log "Hello, World!"
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: PureScript
+Magic Number: 7976
+Time: 2024-11-07T05:03:15.468588
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
