07847b6937ed2ec47d6c3a667117fab4cf9a1ed5
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:05 +0600

    Added Clojure_20241107_050304_720288.clj with Clojure code and metadata

---
+++ Clojure_20241107_050304_720288.clj
@@ -0,0 +1,10 @@
+(println "Hello, World!")
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Clojure
+Magic Number: 6524
+Time: 2024-11-07T05:03:04.721288
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
