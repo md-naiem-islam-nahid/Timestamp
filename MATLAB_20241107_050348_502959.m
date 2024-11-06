63299cf7a72057e3f192ab8a30aaa813ca3dc427
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:48 +0600

    Added MATLAB_20241107_050348_502959.m with MATLAB code and metadata

---
+++ MATLAB_20241107_050348_502959.m
@@ -0,0 +1,10 @@
+disp('Hello, World!');
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: MATLAB
+Magic Number: 3534
+Time: 2024-11-07T05:03:48.502959
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
