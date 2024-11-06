dad0b6293861b3ea0e766b1b3d97f20fe71bf55a
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:45 +0600

    Added Fortran_20241107_050245_032891.f90 with Fortran code and metadata

---
+++ Fortran_20241107_050245_032891.f90
@@ -0,0 +1,12 @@
+program Hello
+  print *, 'Hello, World!'
+end program Hello
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Fortran
+Magic Number: 9401
+Time: 2024-11-07T05:02:45.032891
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
