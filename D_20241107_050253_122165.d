0aa5e9ac6a62770fd484d6fa9cbcf083960d0c38
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:53 +0600

    Added D_20241107_050253_122165.d with D code and metadata

---
+++ D_20241107_050253_122165.d
@@ -0,0 +1,13 @@
+import std.stdio;
+void main() {
+    writeln("Hello, World!");
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: D
+Magic Number: 1633
+Time: 2024-11-07T05:02:53.122165
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
