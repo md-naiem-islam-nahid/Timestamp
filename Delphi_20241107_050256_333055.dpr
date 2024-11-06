383065710e44b77f0b62e2a156f4f7b17e8623a5
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:56 +0600

    Added Delphi_20241107_050256_333055.dpr with Delphi code and metadata

---
+++ Delphi_20241107_050256_333055.dpr
@@ -0,0 +1,13 @@
+program HelloWorld;
+begin
+  WriteLn('Hello, World!');
+end.
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Delphi
+Magic Number: 2703
+Time: 2024-11-07T05:02:56.334056
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
