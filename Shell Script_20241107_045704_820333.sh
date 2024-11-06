072a48e19aa747fe9affc9f5aa87161c76c327fe
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:05 +0600

    Added Shell Script_20241107_045704_820333.sh with Shell Script code and metadata

---
+++ Shell Script_20241107_045704_820333.sh
@@ -0,0 +1,10 @@
+echo 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Shell Script
+Magic Number: 6088
+Time: 2024-11-07T04:57:04.821363
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
