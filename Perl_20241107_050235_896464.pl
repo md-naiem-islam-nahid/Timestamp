c0cf799a7f9d3d1587cc29a36a788595afe4d9ca
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:36 +0600

    Added Perl_20241107_050235_896464.pl with Perl code and metadata

---
+++ Perl_20241107_050235_896464.pl
@@ -0,0 +1,10 @@
+print "Hello, World!\n";
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Perl
+Magic Number: 9100
+Time: 2024-11-07T05:02:35.896464
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
