1f99cd31454753f9da91247cb76f2a68787ee79f
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:49 +0600

    Added OCaml_20241107_050249_006176.ml with OCaml code and metadata

---
+++ OCaml_20241107_050249_006176.ml
@@ -0,0 +1,10 @@
+print_endline "Hello, World!";
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: OCaml
+Magic Number: 5640
+Time: 2024-11-07T05:02:49.007186
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
