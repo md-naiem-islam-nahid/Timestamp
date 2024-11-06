084706b05aa31e008099d1a4b9c916bb0f0db9c5
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:23 +0600

    Added Octave_20241107_050323_052173.m with Octave code and metadata

---
+++ Octave_20241107_050323_052173.m
@@ -0,0 +1,10 @@
+disp('Hello, World!');
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Octave
+Magic Number: 6931
+Time: 2024-11-07T05:03:23.052173
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
