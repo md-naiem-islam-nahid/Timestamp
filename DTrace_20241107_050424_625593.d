4f9117cd04e05fb8d994e54437545d7f9d33ae86
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:25 +0600

    Added DTrace_20241107_050424_625593.d with DTrace code and metadata

---
+++ DTrace_20241107_050424_625593.d
@@ -0,0 +1,10 @@
+dtrace:::BEGIN { trace("Hello, World!"); }
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: DTrace
+Magic Number: 5556
+Time: 2024-11-07T05:04:24.625593
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
