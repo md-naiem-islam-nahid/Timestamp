3d183c87fdcc73cd18f6e3eef4b80ebd39b95003
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:52 +0600

    Added GDScript_20241107_050352_290873.gd with GDScript code and metadata

---
+++ GDScript_20241107_050352_290873.gd
@@ -0,0 +1,11 @@
+func _ready():
+    print("Hello, World!")
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: GDScript
+Magic Number: 1474
+Time: 2024-11-07T05:03:52.290873
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
