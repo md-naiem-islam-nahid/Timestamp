8eecc4dff4ee5ea38e316b220096797115247d83
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:41 +0600

    Added PowerShell_20241107_050241_470592.ps1 with PowerShell code and metadata

---
+++ PowerShell_20241107_050241_470592.ps1
@@ -0,0 +1,10 @@
+Write-Host 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: PowerShell
+Magic Number: 2626
+Time: 2024-11-07T05:02:41.470592
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
