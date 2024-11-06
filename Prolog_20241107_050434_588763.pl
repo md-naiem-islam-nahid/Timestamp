fa52833e186dc6aad71bb1e0c096a74ca19c8ee2
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:35 +0600

    Added Prolog_20241107_050434_588763.pl with Prolog code and metadata

---
+++ Prolog_20241107_050434_588763.pl
@@ -0,0 +1,11 @@
+:- initialization(main).
+main :- write('Hello, World!'), nl.
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Prolog
+Magic Number: 8521
+Time: 2024-11-07T05:04:34.589769
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
