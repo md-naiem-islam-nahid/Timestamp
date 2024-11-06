b7d5f08fe39ce2df31ee421bb67b14d93bfe6b8c
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:13 +0600

    Added Transact-SQL_20241107_050312_835194.sql with Transact-SQL code and metadata

---
+++ Transact-SQL_20241107_050312_835194.sql
@@ -0,0 +1,10 @@
+PRINT 'Hello, World!';
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Transact-SQL
+Magic Number: 6969
+Time: 2024-11-07T05:03:12.835194
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
