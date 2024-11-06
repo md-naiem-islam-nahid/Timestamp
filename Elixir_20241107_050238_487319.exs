2139ab583b544cf4c7d4be715721862d4ae476d3
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:38 +0600

    Added Elixir_20241107_050238_487319.exs with Elixir code and metadata

---
+++ Elixir_20241107_050238_487319.exs
@@ -0,0 +1,10 @@
+IO.puts 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Elixir
+Magic Number: 4955
+Time: 2024-11-07T05:02:38.487319
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
