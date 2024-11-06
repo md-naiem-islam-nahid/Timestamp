c7ff2d80ca953066fccc178be70416d399bd451c
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:56:59 +0600

    Added Lua_20241107_045658_576064.lua with Lua code and metadata

---
+++ Lua_20241107_045658_576064.lua
@@ -0,0 +1,10 @@
+print('Hello, World!')
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Lua
+Magic Number: 4969
+Time: 2024-11-07T04:56:58.576064
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
