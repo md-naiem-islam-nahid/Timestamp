6147ee300c5b685cc755af5b527abe2e7e306411
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:08 +0600

    Added SWI-Prolog_20241107_050407_920981.pl with SWI-Prolog code and metadata

---
+++ SWI-Prolog_20241107_050407_920981.pl
@@ -0,0 +1,11 @@
+:- initialization(main).
+main :- writeln('Hello, World!').
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: SWI-Prolog
+Magic Number: 9581
+Time: 2024-11-07T05:04:07.920981
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
