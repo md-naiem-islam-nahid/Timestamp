84abd1269d63697666cb9b32b9f52ef8a1c33510
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:19 +0600

    Added Zig_20241107_050418_958088.zig with Zig code and metadata

---
+++ Zig_20241107_050418_958088.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+pub fn main() void {
+    std.debug.print("Hello, World!\n", .{});
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Zig
+Magic Number: 2628
+Time: 2024-11-07T05:04:18.958088
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
