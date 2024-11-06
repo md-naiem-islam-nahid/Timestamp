2fe3c7e4ebb633a86366648b073ca24d3ba45738
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:57 +0600

    Added Batch_20241107_050257_461180.bat with Batch code and metadata

---
+++ Batch_20241107_050257_461180.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Hello, World!
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Batch
+Magic Number: 7631
+Time: 2024-11-07T05:02:57.461180
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
