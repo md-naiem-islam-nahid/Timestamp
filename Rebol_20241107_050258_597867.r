d975b5f6cb3c34e9e04fcf7c0696fa0cbb6921cd
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:58 +0600

    Added Rebol_20241107_050258_597867.r with Rebol code and metadata

---
+++ Rebol_20241107_050258_597867.r
@@ -0,0 +1,10 @@
+print "Hello, World!"
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Rebol
+Magic Number: 3066
+Time: 2024-11-07T05:02:58.597867
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
