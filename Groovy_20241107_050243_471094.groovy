63494fbbbd03fcea1410a4948b1efbc060a4e8ae
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:43 +0600

    Added Groovy_20241107_050243_471094.groovy with Groovy code and metadata

---
+++ Groovy_20241107_050243_471094.groovy
@@ -0,0 +1,10 @@
+println 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Groovy
+Magic Number: 7347
+Time: 2024-11-07T05:02:43.471094
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
