64836cea5948d8cf0f06d42082ebcc13ec3c700c
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:58:02 +0600

    Added Bash_20241107_045801_237563.sh with Bash code and metadata

---
+++ Bash_20241107_045801_237563.sh
@@ -0,0 +1,10 @@
+echo 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Bash
+Magic Number: 7937
+Time: 2024-11-07T04:58:01.237563
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
