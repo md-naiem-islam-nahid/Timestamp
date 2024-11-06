c34021bebeb8b07bed4ba52837f554e5f1c7771e
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:03:53 +0600

    Added Arduino_20241107_050353_445034.ino with Arduino code and metadata

---
+++ Arduino_20241107_050353_445034.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, World!");
+}
+void loop() {}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Arduino
+Magic Number: 1152
+Time: 2024-11-07T05:03:53.446052
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
