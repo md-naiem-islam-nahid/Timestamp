dd8c8f436d0656912e6266126c3fb65b7fe17707
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:44 +0600

    Added Erlang_20241107_050244_534432.erl with Erlang code and metadata

---
+++ Erlang_20241107_050244_534432.erl
@@ -0,0 +1,12 @@
+-module(hello).
+-export([start/0]).
+start() -> io:fwrite("Hello, World!\n").
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Erlang
+Magic Number: 2411
+Time: 2024-11-07T05:02:44.534432
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
