2b5dba4fd068c40f9356ce057cffa2701165a414
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:21 +0600

    Added Pascal_20241107_045720_635922.pas with Pascal code and metadata

---
+++ Pascal_20241107_045720_635922.pas
@@ -0,0 +1,13 @@
+program HelloWorld;
+begin
+  writeln('Hello, World!');
+end.
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Pascal
+Magic Number: 6742
+Time: 2024-11-07T04:57:20.636925
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
