f252c993634f896dbfcab2f72857f3058c2aa321
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:56:47 +0600

    Added Java_20241107_045647_097897.java with Java code and metadata

---
+++ Java_20241107_045647_097897.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Java
+Magic Number: 2684
+Time: 2024-11-07T04:56:47.097897
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
