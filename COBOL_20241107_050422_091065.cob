d101327e61fb1cb36d798e2fe3de143eac6a5001
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:04:22 +0600

    Added COBOL_20241107_050422_091065.cob with COBOL code and metadata

---
+++ COBOL_20241107_050422_091065.cob
@@ -0,0 +1,14 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLO-WORLD.
+PROCEDURE DIVISION.
+    DISPLAY 'Hello, World!'.
+    STOP RUN.
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: COBOL
+Magic Number: 8907
+Time: 2024-11-07T05:04:22.091065
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
