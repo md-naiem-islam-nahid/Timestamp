72d7f72977fd6c2c494d0b0b01e403390f88eef4
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:07 +0600

    Added F#_20241107_045707_199326.fs with F# code and metadata

---
+++ F#_20241107_045707_199326.fs
@@ -0,0 +1,10 @@
+printfn "Hello, World!"
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: F#
+Magic Number: 7390
+Time: 2024-11-07T04:57:07.199326
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
