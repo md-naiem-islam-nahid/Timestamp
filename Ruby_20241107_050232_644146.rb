457b32b80035a9c6222ceb3dfa56e889083861f3
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:33 +0600

    Added Ruby_20241107_050232_644146.rb with Ruby code and metadata

---
+++ Ruby_20241107_050232_644146.rb
@@ -0,0 +1,10 @@
+puts 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Ruby
+Magic Number: 6396
+Time: 2024-11-07T05:02:32.644146
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
