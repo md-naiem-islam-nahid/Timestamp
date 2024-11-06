cdb80ca5df19360ea4869fddc3f56fb6a0b405e0
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:52 +0600

    Added Pascal_20241107_050252_587035.pas with Pascal code and metadata

---
+++ Pascal_20241107_050252_587035.pas
@@ -0,0 +1,13 @@
+program HelloWorld;
+begin
+  writeln('Hello, World!');
+end.
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Pascal
+Magic Number: 8669
+Time: 2024-11-07T05:02:52.587035
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
