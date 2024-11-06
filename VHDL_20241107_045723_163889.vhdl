ae8a7d28316546a75f72e8b02c85eb0bbf7183b2
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 04:57:23 +0600

    Added VHDL_20241107_045723_163889.vhdl with VHDL code and metadata

---
+++ VHDL_20241107_045723_163889.vhdl
@@ -0,0 +1,21 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity HelloWorld is
+end HelloWorld;
+architecture Behavioral of HelloWorld is
+begin
+process
+begin
+    report "Hello, World!";
+    wait;
+end process;
+end Behavioral;
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: VHDL
+Magic Number: 7395
+Time: 2024-11-07T04:57:23.163889
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
