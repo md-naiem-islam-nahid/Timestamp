0d2d961018b02c5e122b46bdc457976d885174b5
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:35 +0600

    Added Kotlin_20241107_050234_712557.kt with Kotlin code and metadata

---
+++ Kotlin_20241107_050234_712557.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Hello, World!")
+}
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Kotlin
+Magic Number: 6963
+Time: 2024-11-07T05:02:34.712557
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
