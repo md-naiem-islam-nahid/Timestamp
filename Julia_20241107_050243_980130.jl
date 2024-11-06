51f8ceaebf9cb102ec719ae7d40cae8dd8df46c4
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:44 +0600

    Added Julia_20241107_050243_980130.jl with Julia code and metadata

---
+++ Julia_20241107_050243_980130.jl
@@ -0,0 +1,10 @@
+println("Hello, World!")
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Julia
+Magic Number: 8310
+Time: 2024-11-07T05:02:43.980130
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
