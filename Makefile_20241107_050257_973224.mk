73f1d82973d3ef560b41befda9fa107827c6f778
MD. Naiem Islam Nahid <md.naiem.i.nahid@gmail.com>
2024-11-07 05:02:58 +0600

    Added Makefile_20241107_050257_973224.mk with Makefile code and metadata

---
+++ Makefile_20241107_050257_973224.mk
@@ -0,0 +1,11 @@
+all:
+	@echo 'Hello, World!'
+
+===========================================
+Created by: MD. Naiem Islam Nahid
+File Type: Makefile
+Magic Number: 1805
+Time: 2024-11-07T05:02:57.973224
+Date: Thursday, 07 November 2024, 2024th century
+Emoji: None
+===========================================
